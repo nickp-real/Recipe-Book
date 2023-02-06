1438330623b87546027100a7e60b4869d72f4d72
NickP-real <nick_narak@rocketmail.com>
2023-02-06 12:36:18 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: MainAppBar(drawerKey: _scaffoldKey),
+        centerTitle: true,
+        toolbarHeight: 100,
+        automaticallyImplyLeading: false,
+      ),
+      drawer: Drawer(),
+      body: ListView(
+        children: [
+          Container(
+            color: Color(0xFFD9D9D9),
+            child: ListTile(
+              leading: Container(
+                width: 60,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              title: Text("Tomyum Kung"),
+              subtitle: Text("Thai Cusine"),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+    );
+  }
+}
+
+class MainAppBar extends StatefulWidget {
+  const MainAppBar({super.key, required this.drawerKey});
+
+  final GlobalKey<ScaffoldState> drawerKey;
+
+  @override
+  State<MainAppBar> createState() => _MainAppBarState();
+}
+
+class _MainAppBarState extends State<MainAppBar> {
+  final _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text(
+          "Receipe Book",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          controller: _searchController,
+          style: const TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+              prefixIcon: IconButton(
+                  onPressed: () {
+                    if (widget.drawerKey.currentState!.isDrawerOpen) {
+                      widget.drawerKey.currentState!.closeDrawer();
+                    } else {
+                      widget.drawerKey.currentState!.openDrawer();
+                    }
+                  },
+                  icon: Icon(Icons.menu)),
+              prefixIconColor: Colors.white,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(width: 2)),
+              contentPadding: EdgeInsets.zero,
+              filled: true,
+              fillColor: Colors.black),
+        )
+      ],
+    );
+  }
+}
